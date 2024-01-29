42e3988df8cda86afa86e90f2676ae55e7ca6db7
IrinaZinchenko <zinahohol@gmail.com>
2024-01-29 18:43:19 +0300

    rewrote Board component with using loops

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function Board({xIsNext, squares, onPlay}) {
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
+
     onPlay(nextSquares);
   }
 
@@ -26,24 +28,28 @@ function Board({xIsNext, squares, onPlay}) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  function createGameBoard() {
+    let board = [];
+    const BOARD_SIZE = 3;
+
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      let row = [];
+
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        const index = BOARD_SIZE * i + j
+        row.push(<Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)}/>);
+      }
+
+      board.push(<div key={i} className = "board-row">{row}</div>);
+    }
+
+    return board;
+  }
+
   return (
     <>
       <div className = "status">{status}</div>
-      <div className = "board-row">
-        <Square value = {squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className = "board-row">
-        <Square value = {squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value = {squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value = {squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className = "board-row">
-        <Square value = {squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value = {squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value = {squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+        {createGameBoard()}
     </>
   );
 }
